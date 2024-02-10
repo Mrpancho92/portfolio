2c9655e2cf88005a545ef265a604d0df1db50624
Pavel <133679525+Mrpancho92@users.noreply.github.com>
2024-02-10 15:37:35 +0300

    3

--- src/app/App.js
+++ src/app/App.js
@@ -29,8 +29,8 @@ const App = () => {
                 <main>
                     <Suspense fallback={<Spinner/>}>
                         <Routes>
-                            <Route index element={<Navigate to="/home" replace />} />
-                            <Route path="/home" element={<HomePage />} />
+                            <Route index element={<Navigate to="/portfolio" replace />} />
+                            <Route path="/portfolio" element={<HomePage />} />
                             <Route path="/projects" element={<ProjectPage />} />
                             <Route path="/about" element={ <AboutPage />} />
                             <Route path="/contact" element={<ContactPage />}/>
