d03e298dd362ec2e1f8fe4b6567f1859a10e433d
Pavel <133679525+Mrpancho92@users.noreply.github.com>
2024-02-10 19:52:48 +0300

    77777

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import {HashRouter} from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import {configureStore} from '@reduxjs/toolkit';
 import reducer from './reducer';
-import {HashRouter} from 'react-router-dom';
+
 import {Provider} from 'react-redux';
 // import './index.css';
 import './style/style.scss';
