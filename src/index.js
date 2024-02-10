1e1950b4535fc55295f9215b98fcdf6537031bac
Pavel <133679525+Mrpancho92@users.noreply.github.com>
2024-02-10 19:50:07 +0300

    66666

--- src/index.js
+++ src/index.js
@@ -16,11 +16,13 @@ const store = configureStore({
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
     // <React.StrictMode>
-    <HashRouter>
+    
     <Provider store={store} >
+      <HashRouter>
       <App />
+      </HashRouter>
    </Provider>
-   </HashRouter>
+   
     // </React.StrictMode>
   );
 
