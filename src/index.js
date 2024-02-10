e8d4eea046ad48d997bda346b94a794f6422013f
Pavel <133679525+Mrpancho92@users.noreply.github.com>
2024-02-10 19:45:44 +0300

    gehth

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {configureStore} from '@reduxjs/toolkit';
 import reducer from './reducer';
-
+import {HashRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 // import './index.css';
 import './style/style.scss';
@@ -16,9 +16,11 @@ const store = configureStore({
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
     // <React.StrictMode>
+    <HashRouter>
     <Provider store={store} >
       <App />
    </Provider>
+   </HashRouter>
     // </React.StrictMode>
   );
 
